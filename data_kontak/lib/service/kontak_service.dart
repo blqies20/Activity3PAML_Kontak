e4176e00c13d85b694bd08582392aafc05dd458a
ACER <ACER@DESKTOP-QH5VR0V>
2024-03-31 15:07:51 +0700

    memindahkan bagian getUri dan Future kedalam kurung kurawa daari class KontakService

--- data_kontak/lib/service/kontak_service.dart
+++ data_kontak/lib/service/kontak_service.dart
@@ -4,24 +4,24 @@ import 'package:http/http.dart' as http;
 class KontakService {
   final String baseUrl = 'http://paml.tatiumy.com/api/';
   final String endpoint = 'people';
-}
 
-Uri getUri(String path) {
-  return Uri.parse("$baseUrl$path");
-}
-
-Future<http.Response> addPerson(Map<String, String> data, File? file) async {
-  var request = http.MultipartRequest(
-    'POST',
-    getUri(endpoint),
-  )
-    ..fields.addAll(data)
-    ..headers['Content-Type'] = 'application/json';
-
-  if (file != null) {
-    request.files.add(await http.MultipartFile.fromPath('gambar', file.path));
-  } else {
-    request.files.add(http.MultipartFile.fromString('gambar', ''));
+  Uri getUri(String path) {
+    return Uri.parse("$baseUrl$path");
   }
-  return await http.Response.formStream(await request.send());
+
+  Future<http.Response> addPerson(Map<String, String> data, File? file) async {
+    var request = http.MultipartRequest(
+      'POST',
+      getUri(endpoint),
+    )
+      ..fields.addAll(data)
+      ..headers['Content-Type'] = 'application/json';
+
+    if (file != null) {
+      request.files.add(await http.MultipartFile.fromPath('gambar', file.path));
+    } else {
+      request.files.add(http.MultipartFile.fromString('gambar', ''));
+    }
+    return await http.Response.fromStream(await request.send());
+  }
 }

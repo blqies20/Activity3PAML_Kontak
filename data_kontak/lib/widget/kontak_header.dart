214555d8b8c4cbcb1cccfde29da019328ddbeb9d
ACER <ACER@DESKTOP-QH5VR0V>
2024-04-03 11:12:50 +0700

    add kontak header

---
+++ data_kontak/lib/widget/kontak_header.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class KontakHeader extends StatelessWidget {
+  const KontakHeader({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return AppBar(
+      title: const Center(
+        child: Text("Registration"),
+      ),
+    );
+  }
+}

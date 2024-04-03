0c06c957bf394cdd906d7f3a8f5b50559798da69
ACER <ACER@DESKTOP-QH5VR0V>
2024-04-03 15:02:20 +0700

    add cons

--- data_kontak/lib/widget/kontak_form.dart
+++ data_kontak/lib/widget/kontak_form.dart
@@ -1,6 +1,7 @@
 import 'dart:io';
 
 import 'package:data_kontak/controller/kontak_controller.dart';
+import 'package:data_kontak/screen/homeview.dart';
 import 'package:flutter/material.dart';
 import 'package:data_kontak/model/kontak.dart';
 import 'package:image_picker/image_picker.dart';
@@ -13,7 +14,6 @@ class FormKontak extends StatefulWidget {
 }
 
 class _FormKontakState extends State<FormKontak> {
-
   final _formKey = GlobalKey<FormState>();
   final _namaController = TextEditingController();
   final _emailController = TextEditingController();
@@ -39,69 +39,83 @@ class _FormKontakState extends State<FormKontak> {
   @override
   Widget build(BuildContext context) {
     return Form(
-        key: _formKey,
-        child: SingleChildScrollView(
-          child: Column(
-            children: [
-              Container(
-                margin: EdgeInsets.all(10),
-                child: TextFormField(
-                  decoration: InputDecoration(
-                      labelText: "Nama", hintText: "Masukkan Nama"),
-                  controller: _namaController,
-                ),
+      key: _formKey,
+      child: SingleChildScrollView(
+        child: Column(
+          children: [
+            Container(
+              margin: const EdgeInsets.all(10),
+              child: TextFormField(
+                decoration: const InputDecoration(
+                    labelText: "Nama", hintText: "Masukkan Nama"),
+                controller: _namaController,
               ),
-              Container(
-                margin: EdgeInsets.all(10),
-                child: TextFormField(
-                  decoration: InputDecoration(
-                      labelText: "Email", hintText: "Masukkan Email"),
-                  controller: _emailController,
-                ),
+            ),
+            Container(
+              margin: const EdgeInsets.all(10),
+              child: TextFormField(
+                decoration: const InputDecoration(
+                    labelText: "Email", hintText: "Masukkan Email"),
+                controller: _emailController,
               ),
-              Container(
-                margin: EdgeInsets.all(10),
-                child: TextFormField(
-                  decoration: InputDecoration(
-                      labelText: "Alamat", hintText: "Masukkan Alamat"),
-                  controller: _alamatController,
-                ),
+            ),
+            Container(
+              margin: const EdgeInsets.all(10),
+              child: TextFormField(
+                decoration: const InputDecoration(
+                    labelText: "Alamat", hintText: "Masukkan Alamat"),
+                controller: _alamatController,
               ),
-              Container(
-                margin: EdgeInsets.all(10),
-                child: TextFormField(
-                  decoration: InputDecoration(
-                      labelText: "No Telepon", hintText: "Masukkan No Telepon"),
-                  controller: _noTeleponController,
-                ),
+            ),
+            Container(
+              margin: const EdgeInsets.all(10),
+              child: TextFormField(
+                decoration: const InputDecoration(
+                    labelText: "No Telepon", hintText: "Masukkan No Telepon"),
+                controller: _noTeleponController,
               ),
-              _image == null
-              ? Text("Tidak ada gambar yang dipilih")
-              : Image.file(_image!),
-              ElevatedButton(
-                onPressed: getImage,
-                child: Text("Pilih Gambar"),
-              ),
-              Container(
-                margin: EdgeInsets.all(10),
-                child: ElevatedButton(onPressed: () async {
+            ),
+            _image != null
+                ? Image.file(_image!)
+                : const Text("Tidak ada gambar yang dipilih"),
+            ElevatedButton(
+              onPressed: getImage,
+              child: const Text("Pilih Gambar"),
+            ),
+            Container(
+              margin: const EdgeInsets.all(10),
+              child: ElevatedButton(
+                onPressed: () async {
                   if (_formKey.currentState!.validate()) {
+                    _formKey.currentState!.save();
                     var result = await KontakController().addPerson(
                       Kontak(
-                        nama: _namaController.text,
-                        email: _emailController.text,
-                        alamat: _alamatController.text,
-                        telepon: _noTeleponController.text,
-                        foto: _image!.path
-                      ),
+                          nama: _namaController.text,
+                          email: _emailController.text,
+                          alamat: _alamatController.text,
+                          telepon: _noTeleponController.text,
+                          foto: _image!.path),
                       _image,
                     );
-                    ScaffoldMessenger.of(context).showSnackBar(SnackBar(content: Text(result['message'],),),);
+                    ScaffoldMessenger.of(context).showSnackBar(
+                      SnackBar(
+                        content: Text(
+                          result['message'],
+                        ),
+                      ),
+                    );
+                    Navigator.pushAndRemoveUntil(
+                        context,
+                        MaterialPageRoute(builder: (context) => HomeView()),
+                        (route) => false);
                   }
-                }, child: Text("Simpan")),
-              )
-            ],
-          ),
-        ));
+                },
+                child: const Text("Submit"),
+              ),
+            )
+          ],
+        ),
+      ),
+    );
   }
 }
